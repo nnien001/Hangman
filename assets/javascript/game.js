453812c492a3297f44984d252219903cb07a7cf2
nnien001 <nnien001@gmail.com>
2016-10-10 18:31:04 -0700

    preliminary formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,22 +5,62 @@ console.log('javascript is working');
 		losses: 0,
 		words: [],			//list of possible words game can choose
 		currentWord: "",	//holder for word selected
-		workingWord: [],	//user's working word. 
+		workingWord: [],	//user's working word.
+		workingWordOutput: "", //for cleaner output. 
 		missed: [],
 		missedCount: 0,
 		missLimit: 8,
 		initializeWords: function() {	//organized vertically for ez editing
 			this.words.push(
-				'cat', 
-				'dog', 
-				'horse', 
-				'pig', 
-				'sheep', 
-				'cow', 
-				'goat', 
-				'chicken', 
-				'ostrich', 
-				'alligator'
+				"abyssinian",
+				"american bobtail",
+				"american curl",
+				"american shorthair",
+				"american wirehair",
+				"balinese",
+				"bengal",
+				"birman",
+				"bombay",
+				"british shorthair",
+				"burmese",
+				"burmilla",
+				"chartreux",
+				"chinese li hua",
+				"colorpoint shorthair",
+				"cornish rex",
+				"cymric",
+				"devon rex",
+				"egyptian mau",
+				"european burmese",
+				"exotic",
+				"havana brown",
+				"himalayan",
+				"japanese bobtail",
+				"javanese",
+				"korat",
+				"laperm",
+				"maine coon",
+				"manx",
+				"nebelung",
+				"norwegian forest",
+				"ocicat",
+				"oriental",
+				"persian",
+				"ragamuffin",
+				"ragdoll",
+				"russian blue",
+				"savannah",
+				"scottish fold",
+				"selkirk rex",
+				"siamese",
+				"siberian",
+				"singapura",
+				"snowshoe",
+				"somali",
+				"sphynx",
+				"tonkinese",
+				"turkish angora",
+				"turkish van"
 			);
 			
 			console.log('words initialized');			
@@ -36,31 +76,50 @@ console.log('javascript is working');
 			console.log('missedCount set to 0');
 		},
 
+		initializeMissed: function() {
+			this.missed = [];
+			console.log('missed initialized');
+		},
+
 		initializeWorkingWord: function() {
 			this.workingWord = []; //empty it.
 			for (i = 0; i < this.currentWord.length; i++) {
-				this.workingWord.push("_"); //seed with dummy values
+				if (this.currentWord[i] == " ") {
+					this.workingWord.push(" ");
+				} else {
+					this.workingWord.push("_"); //seed with dummy values
+				}
 			}
+
+			this.updateOutput();
 			console.log("workingWord: " + this.workingWord);
 		},
 
 		initializeAll: function() {
 			this.initializeCurrentWord();
 			this.initializeMissedCount();
+			this.initializeMissed();
 			this.initializeWorkingWord();
 		},
 
 
 		guessSearch: function(x) {
-			var userGuess = x.toLowerCase();
-			var pos = this.currentWord.search(userGuess);
 
-			if (pos == -1) {
-				this.guessWrong(userGuess);
-				
-			}
-			else {
-				this.guessRight(userGuess);
+			//accepts only alpha characters. Else do nothing.
+			if (x.search(/[a-zA-Z]+/) == 0) {
+
+				var userGuess = x.toLowerCase();
+				var pos = this.currentWord.search(userGuess);
+
+				if (pos == -1) {
+					this.guessWrong(userGuess);
+					
+				}
+				else {
+					this.guessRight(userGuess);
+				}
+
+				this.updateOutput();
 			}
 		},
 
@@ -77,9 +136,11 @@ console.log('javascript is working');
 			var pos = this.missed.indexOf(x);
 			if (pos == -1) {
 				this.missed.push(x);
+				this.missedCount += 1
+				
 			}
 			else {
-				this.missedCount += 1
+				//do nothing. they guessed the same character.
 				console.log("missedCount = " + this.missedCount);
 			}
 
@@ -90,7 +151,7 @@ console.log('javascript is working');
 			var pos = this.workingWord.indexOf('_');
 
 			if (pos == -1) {
-				alert('winner');
+				alert('winner! ' + this.currentWord);
 				this.wins += 1;
 				console.log("wins: " + this.wins);
 				this.initializeAll();
@@ -98,19 +159,23 @@ console.log('javascript is working');
 		},
 
 		checkLose: function() {
-			if (this.missedCount > this.missLimit) {
-				alert('Game Over');
+			if (this.missedCount >= this.missLimit) {
+				alert('Game Over. It was ' + this.currentWord);
 				this.losses += 1;
 				console.log("wins: " + this.losses);
 				this.initializeAll();
 			}
 		},
 
+		updateOutput: function() {
+			//output is just workingWord without the array markers
+			this.workingWordOutput = this.workingWord.toString();
+			this.workingWordOutput = this.workingWordOutput.replace(/,/g, "");
 
+			console.log("workingWordOutput: " + this.workingWordOutput);
 
-		touch: function() {
-			console.log('hangman is working');
 		}
+
 
 
 	};
